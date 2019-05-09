9ff885acaca5b1a61557468e2181309e167aac95
Jonathan Selwall <web@selwall.se>
2019-05-10 01:15:57 +0200

    Added so that you can search with commodityCode

--- src/apollo/resolvers/index.js
+++ src/apollo/resolvers/index.js
@@ -4,9 +4,13 @@ const ArticleModel = require('../../mongo/schemas/articleSchema')
 const resolvers = {
   Query: {
     article: async (parent, args, context, info) => {
-      return await ArticleModel.findOne({ id: args.id }).catch(err =>
+      try {
+        const byId = await ArticleModel.findOne({ id: args.id })
+        if (!byId) return await ArticleModel.findOne({ commodityCode: args.id })
+        return byId
+      } catch (err) {
         console.error(err)
-      )
+      }
     },
     allArticles: async (parent, { page, perPage }, context, info) => {
       return await ArticleModel.find()
